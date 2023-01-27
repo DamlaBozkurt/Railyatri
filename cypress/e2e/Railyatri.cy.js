89c3eaeb78f900528acef925e5ddae63f0180860
DamlaBozkurt <120389392+DamlaBozkurt@users.noreply.github.com>
2023-01-27 16:55:51 +0300

    Buy ticket etc
    Bus ticket, train info, sign in, pnr

---
+++ cypress/e2e/Railyatri.cy.js
@@ -0,0 +1,10 @@
+describe('template spec', () => {
+  it('passes', () => {
+    cy.visit('https://www.railyatri.in/')
+    //cy.traintickets()
+    cy.interCitySmartBus()
+    //cy.trainInfo()
+    cy.Signin()
+    cy.Pnr()
+  })
+})
---
+++ cypress/support/commands.js
@@ -0,0 +1,45 @@
+Cypress.Commands.add('traintickets', (trainTickerbooking) => {
+    cy.get('a[href="/train-ticket?utm_source=dwebhome_header_ttb"]').click()
+    cy.get('input[id="boarding_from"]').click( {force: true} ).type('b')
+    cy.get('ul[id="ui-id-1"]').click()
+    cy.get('input[id="boarding_to"]').click().type('g')
+    cy.get('ul[id="ui-id-2"]').click()
+    cy.get('li[data-value="BHOPAL JN | BPL"]').click({force: true})
+    //cy.get('li[class="ui-menu-item"]').should('have.value', 'GORAKHPUR').click()
+    cy.get('div[class="container-fluid"]').eq('1').click({force: true})
+    //cy.get('span[id="ui-id-209"]').click( {force: true})
+    cy.get('li[data-value="GORAKHPUR JN | GKP"]').click({force: true})
+    cy.get('p[class="Next_WD_1"]').click()
+    cy.get('button[class="detail-submit"]').click()
+   // cy.get('div[class="text-center book_now_btn book_tkt_now book_button_12512_3A"]').click()
+})
+
+Cypress.Commands.add('interCitySmartBus', (ticketbooking) => {
+    cy.get('a[class="RY_vertical IntrCity_bus"]').click()
+    cy.get('input[id="id1"]').click( {force: true} ).type('hd')
+    cy.get('li[class="AutoInput_suggestion__Gri5M"]').eq('0').click({force: true})
+    cy.get('input[id="id2"]').click( {force: true} ).type('dd')
+    cy.get('li[class="AutoInput_suggestion__Gri5M"]').eq('0').click({force: true})
+    cy.get('div[class="jsx-4cc9b170fe196755 SearchFormWrapper_futureDateItem__Gv4Tu"]').eq('2').click({force: true})
+    cy.get('button[class="jsx-4cc9b170fe196755 SearchFormWrapper_searchCta__yDxF_"]').click()
+    
+})
+Cypress.Commands.add('trainInfo', (pnrstatus) => {
+    cy.get('span[class="jsx-96366342a1937096 Header_ddBtn__T5q5e"]').eq('0').click()
+    //cy.get('span[class="Header_ryHeadingSpan__ODDbF"]').eq('1').click()
+    cy.get('label[class="jsx-96366342a1937096 Header_dropDown___89lY"]').click({force: true})
+    //cy.get('label[class="jsx-96366342a1937096 Header_dropDown___89lY"]').should('have.value', 'Train information').click()
+    
+})
+Cypress.Commands.add('Signin', (useMobile) => {
+    cy.get('div[class="jsx-96366342a1937096 Header_signInBlock__iByM_"]').click({force: true})
+    cy.get('div[class="jsx-96366342a1937096 Header_loginWrapper__lyqeR"]').click({force: true})
+    cy.get('button[aria-disabled="true"]').contains('Continue').click()
+    cy.get('div[class="jsx-8d37940ae00d92be Auth_autoIn__gyBwD"]').click()
+    cy.get('span[class="jsx-8d37940ae00d92be Auth_closeBtn__qijf_"]').click()
+    })
+    Cypress.Commands.add('Pnr', (pnr) => {
+        cy.get('div[class="jsx-96366342a1937096 Header_logo__Jzm0b"]').click()
+        cy.get('div[id="item"]').click({force: true})
+        cy.get('input[id="search_status"]').click({force: true}).type('334')
+    })
